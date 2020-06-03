a87a938707debaa6fdc03754c6fd49688f24ca0e
Lorena Bucurú <nlbucurur@unal.edu.co>
2020-06-03 12:17:50 -0500

    ESte commit ordenó los archivos en diferentes carpetas, la carpeta "Fotos_color_ConFiltro" deben ir las fotos con el filtro infrarrojo puesto, mientras que la carpeta "Fotos_color_SinFilto". La carpeta "Resultado_resta" es para poner la resta entre la imagen con filtro y la imagen sin filtro, la carpeta "resultado_escala" es cuando ya se coloca la escala de intensidad. Algo que hay que arreglar o descubrir, es la forma de alinear las imagenes para no tener corrimiento en la resta.

--- img.py
+++ img.py
@@ -5,8 +5,8 @@ import pandas as pd
 import numpy as np
 import cv2
 
-color = cv2.imread("color.jpg")
-infrared = cv2.imread("infrared.jpg")
+color = cv2.imread("Fotos_color_ConFiltro/color.jpg")
+infrared = cv2.imread("Fotos_color_SinFiltro/infrared.jpg")
 
 gray_color = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
 gray_infrared = cv2.cvtColor(infrared, cv2.COLOR_BGR2GRAY)
@@ -18,7 +18,7 @@ result = result * 1.3715555 + 25.4
 w = result.shape[1]
 h = result.shape[0]
 
-cv2.imwrite("result.jpg", result)
+cv2.imwrite("Resultado_resta/result.jpg", result)
 
 image_dict = {}
 
@@ -27,7 +27,7 @@ for col in range(w):
 
 image_data = pd.DataFrame(image_dict)
 
-image_data.to_csv("image_data.csv")
+image_data.to_csv("Numeric_data/image_data.csv")
 
 Z = result[int(4*h/10):int(6*h/10), int(4*w/10):int(5*w/8)]
 X = range(Z.shape[1])
@@ -40,4 +40,4 @@ color_map = plt.imshow(Z)
 color_map.set_cmap("hot")
 fig.colorbar(color_map, ax=ax, extend="both")
 plt.show()
-plt.savefig("out.pdf")
+plt.savefig("Resultado_escala/out.pdf")
