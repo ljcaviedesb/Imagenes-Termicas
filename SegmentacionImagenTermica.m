2a29aa954e18579059056a2a47291cebacfe3d5e
ljcaviedesb <ljcaviedesb@unal.edu.co>
2020-06-03 20:38:15 -0500

    Add files via upload
    Recorte de rostros para análisis de este únicamente

--- SegmentacionImagenTermica.m
+++ SegmentacionImagenTermica.m
@@ -3,10 +3,22 @@
 clc; clear;
 % Lectura de la imagen térmica y sus características
 Ii = imread('andres4.jpg');subplot(1,3,1);imshow(Ii);title('Imagen sin filtro IR(Ii)');
-Ic=imread('andres3.jpg');subplot(1,3,2);imshow(Ic);title('Imagen con filtro IR(Ic)');
+Ic=imread('andres1.jpg');subplot(1,3,2);imshow(Ic);title('Imagen con filtro IR(Ic)');
 Iig = rgb2gray(Ii); Icg = rgb2gray(Ic);
 II = imsubtract(Ii,Ic);
-I = imsubtract(Iig,Icg); figure;imshow(I);title('Imagen resta Ii-Ic GRAY');
+I = imsubtract(Iig,Icg); subplot(1,3,3);imshow(I);title('Imagen resta Ii-Ic GRAY');
+%% Recortar rostro de una imágen
+% detector de objetos
+DetectorRostro = vision.CascadeObjectDetector('FrontalFaceCART');   
+BBOX = step(DetectorRostro,Is)
+
+% Detecta rostros
+posicion = step(DetectorRostro, Is)
+
+%r = centerCropWindow2d(size(Reff),posicion);
+I = imcrop(Is,BBOX(1,:));
+figure;imshow(I)
+%% 
 improfile
  [x,y] = size(I);
  X=1:x;
@@ -18,17 +30,14 @@ improfile
 %figure; imshow(i);title('Imagen resta Ii-Ic DOUBLE');
 %figure;imhist(I);title('imhist(Irg)')
  
-whos I
-% Computar el rango de la matriz para ver el rango de temperatura de la imágen
-rango = [median(I(:)) max(I(:))];
 
  %% Filtros de colores
  % NO ES NECESARIO CORRER ESTA PARTE DEL CODIGO
   % utilizo la función colorfilter creada para filtrar el color rojo, azul y
   % verde de cada imágen
   %subplot(3,1,1)
-  figure;
- Ir = colorfilter(II,'r'); title('Icolor filtro rojo')% filtro de la imagen a color
+ % figure;
+ %Ir = colorfilter(II,'r'); title('Icolor filtro rojo')% filtro de la imagen a color
  %subplot(3,1,2)
 %  figure;
 % Ig = colorfilter(II,'g');title('Icolor filtro verde')
@@ -36,6 +45,11 @@ rango = [median(I(:)) max(I(:))];
 %  figure;
 % Ib = colorfilter(II,'b');title('Icolor filtro azul')
 %%  Remover el ruido preservando los detalles importantes de la imagen
+
+whos I
+% Computar el rango de la matriz para ver el rango de temperatura de la imágen
+rango = [median(I(:)) max(I(:))];
+
 ValorSuave = 0.01*diff(rango).^2;
 %Como X=RANGO es un vector de longitud m=2, Y=diff(X) devuelve un vector de
 %longitud m-1=2-1=1. Los elementos de Y son las diferencias entre los 
@@ -50,7 +64,7 @@ figure; imshow(J,[]);colormap(gca,hot);
 
 % % title('Imagen Filtrada Guiada')
 %% Determinar los valores limites para usar en la segmentación
-umbral = multithresh(0.78*J,2)
+umbral = multithresh(J,1)
 
 % el metodo Otsu es para segmentar
 % Devuelve un vector de 1 por N que contiene los valores de umbral 
